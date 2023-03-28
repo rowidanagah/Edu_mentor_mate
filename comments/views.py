59236fc89d75562ae18226143ea34b9a094d3d4a
Rowida46 <rowidanagah462000@gmail.com>
2023-03-28 02:55:59 +0200

    adding delete & update comments

--- comments/views.py
+++ comments/views.py
@@ -3,6 +3,8 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from .models import Comment
 from .serializer import CommentSerializer
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class CommentList(ListCreateAPIView):
@@ -13,3 +15,16 @@ class CommentList(ListCreateAPIView):
 class CommentDetail(RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+    def put(self, request, *args, **kwargs):
+        comment = self.get_object()
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, *args, **kwargs):
+        comment = self.get_object()
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
