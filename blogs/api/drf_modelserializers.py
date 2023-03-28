261b5ed31e273e0807561ace28c8709d782af5d1
OsamaAhmed187 <osamaahmedalkatkoti@gmail.com>
2023-03-28 19:16:24 +0200

    add filter and search to blog

--- blogs/api/drf_modelserializers.py
+++ blogs/api/drf_modelserializers.py
@@ -1,13 +1,19 @@
 from blogs.api.serializers import BlogModelSerializer
 from blogs.models import BLog
 from rest_framework.generics import ListCreateAPIView ,RetrieveUpdateDestroyAPIView
-
+from rest_framework.filters import  SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 # =============================(list)====================================
 class bloglist(ListCreateAPIView):
     serializer_class = BlogModelSerializer
     queryset = BLog.objects.all()
+    filter_backends = [DjangoFilterBackend,SearchFilter]
+    filterset_fields = ['title']
+    search_fields = ['title']
 
 # =============================(RetrieveUpdateDestroyAPIView)============
 class BlogRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = BlogModelSerializer
     queryset = BLog.objects.all()
+
+# =============================(Filter )=================================
