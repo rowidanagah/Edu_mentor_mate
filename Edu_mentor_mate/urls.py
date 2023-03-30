fc4ff16bf29892ae12c017cd5146dce8f1086980
Rowida46 <rowidanagah462000@gmail.com>
2023-03-30 06:46:31 +0200

    working on blog

--- Edu_mentor_mate/urls.py
+++ Edu_mentor_mate/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 # ==(osama)====
@@ -34,4 +35,4 @@ urlpatterns = [
 
 ]
 # ===(osama)===
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
