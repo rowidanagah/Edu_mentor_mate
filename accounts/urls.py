dd7205c3cd361e95fa160e667872730dfd2ec347
Abdallah <Abdallah97158@gmail.com>
2023-03-30 09:07:22 +0200

    stop email verication temp

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 from dj_rest_auth.registration.views import VerifyEmailView
 urlpatterns = [
 	path('register', views.UserRegister.as_view(), name='register'),
-	path('login', views.UserLogin.as_view(), name='login'),
+	# path('login', views.UserLogin.as_view(), name='login'),
 	path('update', views.UpdateUser.as_view(), name='update'),
 	path('logout', views.UserLogout.as_view(), name='logout'),
 	path('user', views.UserView.as_view(), name='user'),
