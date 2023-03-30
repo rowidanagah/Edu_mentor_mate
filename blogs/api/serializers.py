262fd8a252cf947f9353fe94ffca1cfaaaad1602
Rowida46 <rowidanagah462000@gmail.com>
2023-03-30 06:46:31 +0200

    working on blog

--- blogs/api/serializers.py
+++ blogs/api/serializers.py
@@ -7,16 +7,15 @@ from roomsession.serializers import BlogSessionSerializer
 class BlogModelSerializer(serializers.ModelSerializer):
     mentor = UserSerializer()
     session = BlogSessionSerializer()
-    cover_image = serializers.SerializerMethodField()
-
+    cover_image = serializers.ImageField(required=False)
     class Meta:
         model = BLog
         fields = ('id', 'title', 'content', 'mentor', 'updated_at',
                   'cover_image', 'created_at', 'tags', 'session', 'updated_at')
 
-    def get_cover_image(self, obj):
-        if obj.cover_image:
-            print('------------------', self.context['request'].build_absolute_uri(obj.cover_image.url))
+    # def get_cover_image(self, obj):
+    #     if obj.cover_image:
+    #         print('------------------', self.context['request'].build_absolute_uri(obj.cover_image.url))
 
-            return self.context['request'].build_absolute_uri(obj.cover_image.url)
-        return ''
+    #         return self.context['request'].build_absolute_uri(obj.cover_image.url)
+    #     return ''
