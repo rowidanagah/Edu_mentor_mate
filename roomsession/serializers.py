82a984311fe3305727125f9a3e141b122aa07f80
Rowida46 <rowidanagah462000@gmail.com>
2023-03-30 03:34:47 +0200

    fetch blogs

--- roomsession/serializers.py
+++ roomsession/serializers.py
@@ -11,15 +11,15 @@ class SessionDateSerializer(serializers.ModelSerializer):
 
 class SessionSerializer(serializers.ModelSerializer):
     available_dates = SessionDateSerializer(many=True, read_only=True)
-    mentor=UserSerializer()
+    mentor = UserSerializer()
     # user_bio = serializers.SerializerMethodField(read_only=True)
     # bio = serializers.CharField(source='bio', read_only=True)
 
     class Meta:
         model = RoomSession
         fields = ('title', 'available_dates', 'mentor',
-                  'ended_at', 'sessionUrl', 'tags', 'deruration','updated_at')
-        #depth = 1
+                  'ended_at', 'sessionUrl', 'tags', 'deruration', 'updated_at')
+        # depth = 1
 
     # def create(self, validated_data):
     #     print("-------------validation------", validated_data)
@@ -27,3 +27,14 @@ class SessionSerializer(serializers.ModelSerializer):
     #     print("-------------------pop----------------", dates)
     #     session_rom = RoomSession.objects.create(**validated_data)
     #     return session_rom
+
+
+class BlogSessionSerializer(serializers.ModelSerializer):
+    available_dates = SessionDateSerializer(many=True, read_only=True)
+    # user_bio = serializers.SerializerMethodField(read_only=True)
+    # bio = serializers.CharField(source='bio', read_only=True)
+
+    class Meta:
+        model = RoomSession
+        fields = ('title', 'available_dates',
+                  'ended_at', 'sessionUrl', 'tags', 'deruration', 'updated_at')
