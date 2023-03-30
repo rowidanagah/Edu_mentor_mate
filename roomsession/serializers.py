3fa722c3faefb764c193adcb17128a278fb6bf9b
hp <kareemtall99@gmail.com>
2023-03-30 05:11:43 +0200

    time format

--- roomsession/serializers.py
+++ roomsession/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from roomsession.models import RoomSession, SessionDate
 from accounts.serializers import UserSerializer
+from tags.models import Tags
+from django.utils import timezone
+from datetime import timedelta
 
 
 class SessionDateSerializer(serializers.ModelSerializer):
@@ -8,17 +11,44 @@ class SessionDateSerializer(serializers.ModelSerializer):
         model = SessionDate
         fields = '__all__'
 
+# class TagSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Tags
+#         fields = ('caption',)
+
 
 class SessionSerializer(serializers.ModelSerializer):
     available_dates = SessionDateSerializer(many=True, read_only=True)
+    # tags=TagSerializer(many=True, read_only=True)
     mentor=UserSerializer()
+    created_at = serializers.DateTimeField(format='%d %b')
     # user_bio = serializers.SerializerMethodField(read_only=True)
     # bio = serializers.CharField(source='bio', read_only=True)
+    time_since_created = serializers.SerializerMethodField()
+
+    def get_time_since_created(self, obj):
+        now = timezone.now()
+        time_since = now - obj.created_at
+        if time_since < timedelta(minutes=1):
+            return "just now"
+        elif time_since < timedelta(hours=1):
+            minutes = int(time_since.total_seconds() / 60)
+            return f"{minutes} min ago"
+        elif time_since < timedelta(days=1):
+            hours = int(time_since.total_seconds() / 3600)
+            return f"{hours} hours ago"
+        else:
+            days = int(time_since.total_seconds() / 86400)
+            return f"{days} days ago"
+
 
     class Meta:
         model = RoomSession
         fields = ('title', 'available_dates', 'mentor',
-                  'ended_at', 'sessionUrl', 'tags', 'deruration','updated_at')
+                  'ended_at', 'sessionUrl', 'tags', 'deruration','updated_at','created_at','time_since_created',)
+        
+        
+      
         #depth = 1
 
     # def create(self, validated_data):
