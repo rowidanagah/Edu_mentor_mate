0a39d670a7c92caa0dc60114ff8cfcbca3f331a9
Rowida46 <rowidanagah462000@gmail.com>
2023-04-06 03:02:33 +0200

    session feedback

--- reactions/urls.py
+++ reactions/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import LikeAPIView, followAPIView
+from .views import LikeAPIView, followAPIView, SessionFeedbackAPIView, SessionFeedbackCreateApi
 
 from roomsession.views import session_list
 urlpatterns = [
     path('like/', LikeAPIView.as_view(), name='like'),
     path('follow/', followAPIView.as_view(), name='like'),
+    path('SessionFeedback/', SessionFeedbackAPIView.as_view(), name='blog-list'),
+    path('SessionFeedback/create/',
+         SessionFeedbackCreateApi.as_view(), name='blog-create'),
 
 ]
