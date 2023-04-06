3e51b4d2dd2f00ec5a78bd681b16fa2e9876a549
Rowida46 <rowidanagah462000@gmail.com>
2023-04-06 08:53:05 +0200

    adding session picked by user

--- roomsession/urls.py
+++ roomsession/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from roomsession.views import *
 
 urlpatterns = [
-    path('',session_list),
+    path('', session_list),
     path('<int:pk>', session_detail),
-    path('session-date/<int:pk>',singleDateUpdateView.as_view())
+    path('session-date/<int:pk>', singleDateUpdateView.as_view()),
+    path('user_picked_sessions/', UserPickedSessionsView.as_view(),
+         name='picked-session-list'),
 
-] 
+
+]
