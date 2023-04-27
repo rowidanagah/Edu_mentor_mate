d595da0b9340f2a3fb683bd562094bb0f57a872b
Rowida46 <rowidanagah462000@gmail.com>
2023-04-28 02:36:59 +0300

    fix app init

--- Edu_mentor_mate/wsgi.py
+++ Edu_mentor_mate/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Edu_mentor_mate.settings')
 
+
 application = get_wsgi_application()
+app = application
